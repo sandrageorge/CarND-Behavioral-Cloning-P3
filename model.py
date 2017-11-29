383ce09eee127736334acac9e3feb331295cddcd
sandrageorge <sandra.george@valeo.com>
2017-11-29 14:56:52 +0200

    Final model changes, video uploaded

--- model.py
+++ model.py
@@ -2,39 +2,12 @@ import csv
 import numpy as np
 from click.core import batch
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Dropout, Cropping2D, Activation
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Dropout, Cropping2D, Conv2D
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from keras.layers.advanced_activations import ELU
 from keras.optimizers import Adam
 import cv2
-
-# lines = []
-# firstline = True
-# with open('data/data/driving_log.csv') as csvfile:
-#     reader = csv.reader(csvfile)
-#     for line in reader:
-#         if firstline:  # skip first line
-#             firstline = False
-#             continue
-#         lines.append(line)
-#
-# images = []
-# measurements = []
-#
-# for line in lines:
-#     source_path = line[0]
-#     filename = source_path.split('/')[-1]
-#     current_path = 'data/data/IMG/' + filename
-#     image = cv2.imread(current_path)
-#     images.append(image)
-#
-#     measurement = float(line[3])
-#     measurements.append(measurement)
-#
-# X_train = np.array(images)
-# y_train = np.array(measurements)
-
 
 def generate_samples(paths):
     samples = []
@@ -43,8 +16,7 @@ def generate_samples(paths):
         with open(csv_path) as csvfile:
             reader = csv.reader(csvfile)
             for i, line in enumerate(reader):
-                if i > 0:
-                    samples.append(line)
+                samples.append(line)
 
     print("Testing data number:", len(samples))
 
@@ -63,7 +35,7 @@ def generator(samples, split_str, batch_size_=64):
             angles = []
             for batch_sample in batch_samples:
                 source_path = batch_sample[0]
-                current_path = '..' + source_path.split(split_str)[-1].replace('\\', '/')
+                current_path = '../../' + source_path.split(split_str)[-1].replace('\\', '/')
                 # current_path = '../track1/data/data/' + source_path
 
                 image = cv2.imread(current_path)
@@ -72,22 +44,22 @@ def generator(samples, split_str, batch_size_=64):
                 angles.append(angle)
 
                 source_path = batch_sample[1]
-                current_path = '..' + source_path.split(split_str)[-1].replace('\\', '/')
+                current_path = '../../' + source_path.split(split_str)[-1].replace('\\', '/')
                 # current_path = '../track1/data/data/' + source_path
 
                 image = cv2.imread(current_path)
                 angle = float(batch_sample[3])
                 images.append(image)
-                angles.append(angle+0.2)
+                angles.append(angle+correction)
 
                 source_path = batch_sample[2]
-                current_path = '..' + source_path.split(split_str)[-1].replace('\\', '/')
+                current_path = '../../' + source_path.split(split_str)[-1].replace('\\', '/')
                 # current_path = '../track1/data/data/' + source_path
 
                 image = cv2.imread(current_path)
                 angle = float(batch_sample[3])
                 images.append(image)
-                angles.append(angle-0.2)
+                angles.append(angle-correction)
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -126,43 +98,26 @@ def LeNet():
     leNetModel.add(Dense(1))
     return leNetModel
 
-def Nvidia(input_shape=(160, 320, 3)):
-    nVidia = Sequential()
-    nVidia.add(Cropping2D(cropping=((40,20),(0,0)), input_shape=(160,320,3)))
-    nVidia.add(Lambda(lambda x: x / 255.0 - 0.5, name="image_normalization", input_shape=input_shape))
-
-    nVidia.add(Convolution2D(24, 5, 5, name="convolution_1", subsample=(2, 2), border_mode="valid", init='he_normal'))
-    nVidia.add(ELU())
-    nVidia.add(Convolution2D(36, 5, 5, name="convolution_2", subsample=(2, 2), border_mode="valid", init='he_normal'))
-    nVidia.add(ELU())
-    nVidia.add(Convolution2D(48, 5, 5, name="convolution_3", subsample=(2, 2), border_mode="valid", init='he_normal'))
-    nVidia.add(ELU())
-    nVidia.add(Convolution2D(64, 3, 3, name="convolution_4", border_mode="valid", init='he_normal'))
-    nVidia.add(ELU())
-
-    nVidia.add(Convolution2D(64, 3, 3, name="convolution_5", border_mode="valid", init='he_normal'))
-    nVidia.add(ELU())
-    nVidia.add(Flatten())
-
-    nVidia.add(Dropout(0.5))
-
-    nVidia.add(Dense(100, name="hidden1", init='he_normal'))
-    nVidia.add(ELU())
-
-    nVidia.add(Dropout(0.5))
-
-    nVidia.add(Dense(50, name="hidden2", init='he_normal'))
-    nVidia.add(ELU())
-    nVidia.add(Dense(10, name="hidden3", init='he_normal'))
-    nVidia.add(ELU())
-
-    nVidia.add(Dense(1, name="steering_angle", activation="linear"))
-
-    return nVidia
+def Nvidia():
+    model = Sequential()
+    model.add(Cropping2D(cropping=((40, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(64, 64, 3)))
+    model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
 
 epochs = 30
 batch_size = 8
-model = Nvidia(input_shape=(160, 320, 3))
+model = Nvidia()
 #model = LeNet()
 # model = InitialModel()
 
@@ -172,14 +127,14 @@ model = Nvidia(input_shape=(160, 320, 3))
 # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=epochs)
 
 # paths, split_str = ['../track1/2labs_center/', '../track1/1lab_recovery/', '../track1/1lab_smothcurve/', '../track1/FirstRecord/', '../track1/Recover/', '../track1/Recover2/'], 'Behavioral_cloning'
-paths, split_str = ['../track1/data/data/'], 'Behavioral_cloning'
+paths, split_str = ['../../track1/data/'], '07_Behavioral_Cloning'
 samples = generate_samples(paths)
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_samples, validation_samples = train_test_split(samples, test_size=0.15)
 path = []
 train_generator = generator(train_samples, split_str=split_str, batch_size_=batch_size)
 validation_generator = generator(validation_samples, split_str=split_str, batch_size_=batch_size)
 
-model.compile(optimizer=Adam(lr=0.001), loss='mse')
+model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator,
                                      steps_per_epoch=len(train_samples) / batch_size,
                                      validation_data=validation_generator,
@@ -188,7 +143,9 @@ history_object = model.fit_generator(train_generator,
 
 
 
-model.save('models/nVidia_side_records.h5')
+save_str = "Nvidia" + '_e' + str(epochs) + '_b' + str(batch_size) + '.h5'
+model.save(save_str)
+print(save_str)
 
 # plot the training and validation loss for each epoch
 from matplotlib import pyplot as plt
